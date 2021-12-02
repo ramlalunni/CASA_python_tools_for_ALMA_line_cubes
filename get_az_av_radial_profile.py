3ba453ad2f9e1fad1fe5df59a41a24e9ea2cb950
Ramlal Unnikrishnan <ramlal.unnikrishnan@chalmers.se>
2021-12-02 19:42:44 +0100

    Created get_az_av_radial_profile.py

--- get_az_av_radial_profile.py
+++ get_az_av_radial_profile.py
@@ -12,55 +12,55 @@ import numpy as np
 import matplotlib.pyplot as plt
 from astropy.io import fits
 
-###function to generate azimuthally averaged radial profiles (ASCII and PDF)
+# function to generate azimuthally averaged radial profiles (ASCII and PDF)
 ###----------------------------------------------------------------------------###
 def plot_az_av_rad_prof(cube, min_radius, max_radius, center, center_channel, binsize):
     """function to plot azimuthally averaged radial profile of a channel from the line cube.
        simplified adaptation of 'radialprofile.py' by Adam Ginsburg.
        See <https://github.com/keflavich/image_tools/blob/793e93065afe2754a818da8b58f9b222a3acf59f/image_tools/radialprofile.py>"""
 
-    ###reading header and data from fits cube
+    # reading header and data from fits cube
     hdulist = fits.open('%s' %cube)
     data = hdulist[0].data
     header = fits.getheader('%s' %cube)
 
-    ###defining pixel coordinates of image center, if not already input to function
+    # defining pixel coordinates of image center, if not already input to function
     CRPIX1 = header['CRPIX1']
     CRPIX2 = header['CRPIX2']
     if center is None:
         center = [int(CRPIX1) - 1, int(CRPIX2) - 1]
 
-    ###calculating pixel size in arcesconds
+    # calculating pixel size in arcesconds
     CDELT1 = header['CDELT1']
     pixel_size = 3600 * abs(CDELT1)
 
-    ###finding central channel image
+    # finding central channel image
     center_image = data[center_channel]
 
-    ###calculating image indices
+    # calculating image indices
     y, x = np.indices(center_image.shape)
     r = np.hypot(x - center[0], y - center[1])
 
-    ###defining binning parameters
+    # defining binning parameters
     nbins = int(np.round(r.max() / binsize)+1)
     maxbin = nbins * binsize
     bins = np.linspace(0,maxbin,nbins+1)
     bin_centers = (bins[1:]+bins[:-1])/2.0
 
-    ###calculating azimuthal profile and radii
+    # calculating azimuthal profile and radii
     azimuthal_profile = np.histogram(r, bins, weights=center_image)[0]/np.histogram(r, bins)[0]
     radii = bin_centers*pixel_size
 
-    ###removing trailing NaNs
+    # removing trailing NaNs
     azimuthal_profile = azimuthal_profile[np.logical_not(np.isnan(azimuthal_profile))]
     radii = radii[:len(azimuthal_profile)]
 
-    ###saving azimuthal profile and radius values to text file
+    # saving azimuthal profile and radius values to text file
     azimuthal_profile_data = np.column_stack([radii, azimuthal_profile])
     profile_saving_path = 'ascii_az_av_radial_profiles/%s_az_av_rad_profile.txt' %cube[:-5]
     np.savetxt(profile_saving_path , azimuthal_profile_data, fmt=['%6.3f','%10.8e'])
 
-    ###selecting only pixels within the given input range of minimum and maximum radii
+    # selecting only pixels within the given input range of minimum and maximum radii
     lower_boundary = 0
     flipped_radii = np.flip(radii)
     for i in range(len(flipped_radii)):
@@ -76,7 +76,7 @@ def plot_az_av_rad_prof(cube, min_radius, max_radius, center, center_channel, bi
     selected_azimuthal_profile = azimuthal_profile[lower_boundary:upper_boundary]
     print('Radial profile from %s to %s arcseconds will be plotted\n' %(min_radius, max_radius))
 
-    ###plotting azimuthally averaged radial profile
+    # plotting azimuthally averaged radial profile
     print('Plotting azimuthally averaged radial profile of %s\n' %cube)
     fig1, ax1 = plt.subplots()
     ax1.plot(selected_radii, selected_azimuthal_profile)
