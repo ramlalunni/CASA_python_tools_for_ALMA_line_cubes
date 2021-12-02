95b137fcaf27f696ef9ca060c3917382eb84d2c2
Ramlal Unnikrishnan <ramlal.unnikrishnan@chalmers.se>
2021-12-02 19:24:43 +0100

    Updated get_spectra.py

--- get_spectra.py
+++ get_spectra.py
@@ -1,3 +1,24 @@
+# get_spectra.py
+
+########################################################################
+# Copyright (C) 2021  Ramlal Unnikrishnan
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# contact: ramlal.unnikrishnan@chalmers.se
+########################################################################
+
 import numpy as np
 import matplotlib.pyplot as plt
 import os
