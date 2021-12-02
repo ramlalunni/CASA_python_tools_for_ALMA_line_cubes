ac0b56be0139322b8ea2604ba99ec294c3b0b1d1
Ramlal Unnikrishnan <ramlal.unnikrishnan@chalmers.se>
2021-12-02 22:52:38 +0100

    Updated channel_map_plotter.py

--- channel_map_plotter.py
+++ channel_map_plotter.py
@@ -30,10 +30,16 @@ import os
 
 from modifier_functions import ChangeAxisColor
 
-###function to generate tiled channel maps (PDF)
+# function to generate tiled channel maps (PDF)
 ###----------------------------------------------------------------------------###
 def plot_channel_maps(cube, imsize, tick_size, v_sys, sig):
-    """docstring"""
+    """Plots 5x5 tiled channel map of line cube, with user specified contour level overplotted.
+       Inputs: 1. cube (str): FITS filename, Eg: 'IRAS_07454-7112_100.076392GHz_HC3N_a3.fits'
+               2. imsize (int): size (side of square) of channel region to plot (arcseconds), Eg: 25
+               3. tick_size (int): interval at which to put axes ticks (arcseconds), Eg: 10
+               4. v_sys (float): systemic velocity of the source, Eg: -38.70
+               5. sig (int): sigma level at which to plot contour, Eg: 5
+               """
 
     # creating necessary directories (if not already present)
     dirs_to_create = ['channel_maps/']
@@ -41,31 +47,46 @@ def plot_channel_maps(cube, imsize, tick_size, v_sys, sig):
         if not os.path.exists('%s' %dir):
             os.makedirs('%s' %dir)
 
-    ###finding central channel of line cube (channel at systemic velocity)
-    center_channel = np.argmin(np.abs(vels-v_sys))
-
-    ###reading header, data and beams from fits cube
+    # reading header, data and beams from fits cube
     hdulist = fits.open('%s' %cube)
     header = fits.getheader('%s' %cube)
     data = hdulist[0].data
     beams   = hdulist[1].data
 
-    ###pixel references
-    CRPIX1 = header['CRPIX1'] ###RA reference pixel
-    CDELT1 = header['CDELT1']  ###RA pixel width (degrees)
-    CRPIX2 = header['CRPIX2'] ###DEC reference pixel
-    CDELT2 = header['CDELT2']  ###DEC pixel width (degrees)
-
-    ###finding center pixel
+    # velocity references
+    NAXIS3 = header['NAXIS3']
+    CTYPE3 = header['CTYPE3']
+    CRVAL3 = header['CRVAL3']
+    CDELT3 = header['CDELT3']
+    CRPIX3 = header['CRPIX3']
+    
+    # finding channel velocities
+    if CTYPE3 == 'VRAD':
+        vels = np.zeros(NAXIS3)
+        for i in range(NAXIS3):
+            vels[i] = CRVAL3 + CDELT3*(i - CRPIX3 + 1)
+    else:
+        raise TypeError('AXIS3 is not in velocity units')
+        
+    # pixel references
+    CRPIX1 = header['CRPIX1'] # RA reference pixel
+    CDELT1 = header['CDELT1'] # RA pixel width (degrees)
+    CRPIX2 = header['CRPIX2'] # DEC reference pixel
+    CDELT2 = header['CDELT2'] # DEC pixel width (degrees)
+     
+    # finding central channel of line cube (channel at systemic velocity)
+    center_channel = np.argmin(np.abs(vels-v_sys))
+    
+    # finding center pixel
     center = [int(CRPIX1 - 1), int(CRPIX2 - 1)]
 
-    ###calculating pixel size in arcseconds
+    # calculating pixel size in arcseconds
     pixel_size = 3600 * abs(CDELT1)
 
-    ###intensity unit reference
+    # intensity unit reference
     BUNIT = header['BUNIT']
 
-    ##defining and adjusting figure and subplots
+    # defining and adjusting figure and subplots
     nrows = 5
     ncols = 5
     fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=(10, 10))#, sharex=True, sharey=True)
@@ -74,40 +95,40 @@ def plot_channel_maps(cube, imsize, tick_size, v_sys, sig):
     fig.subplots_adjust(wspace=-0.1)
     fig.subplots_adjust(hspace=0.0)
 
-    ###choosing tile size and location of ticks
-    tick_size_pix = tick_size/(3600 * abs(CDELT1)) ###pixels
+    # choosing tile size and location of ticks
+    tick_size_pix = tick_size/(3600 * abs(CDELT1)) # pixels
     tick_locs = [-tick_size_pix, 0, tick_size_pix]
     x_tick_labels = np.round([3600 * i * CDELT1 for i in tick_locs],decimals=1)
     y_tick_labels = np.round([3600 * i * CDELT2 for i in tick_locs],decimals=1)
     x_tick_labels[1] = np.abs(x_tick_labels[1])
     y_tick_labels[1] = np.abs(y_tick_labels[1])
 
-    ###defining plotting extent and upper and lower limits
+    # defining plotting extent and upper and lower limits
     extent = int(0.5*imsize/(3600*abs(CDELT1)))
     low_lim = int(CRPIX1)-extent
     upp_lim = int(CRPIX1)+extent
 
-    ###defining colourmap for imshow
+    # defining colourmap for imshow
     cmap = plt.get_cmap('cubehelix')
 
-    ###printing calculated cube details to terminal
+    # printing calculated cube details to terminal
     print('Channel velocities are %s' %vels)
     print('Center pixel of channels is %s' %center)
     print('Pixel size is %s arcseconds\n' %'{0:.3f}'.format(pixel_size))
 
-    ###choosing number of channels to skip at the beginning of cube
+    # choosing number of channels to skip at the beginning of cube
     skip_chans = center_channel - int((nrows*ncols - 1)/2)
     last_chan = center_channel + int((nrows*ncols - 1)/2)
 
-    ###finding maximum intensity to plot for setting colourbar contrast
+    # finding maximum intensity to plot for setting colourbar contrast
     max_int = 0.0
     for chan in range(skip_chans, last_chan+1):
         if np.nanmax(data[chan]) > max_int:
             max_int = np.nanmax(data[chan])
 
-    ###finding minimum intensity to plot for setting colourbar contrast
+    # finding minimum intensity to plot for setting colourbar contrast
     min_int = 0.0
-    boundary = 25 ###arcsec
+    boundary = 25 # arcsec
     y, x = np.indices(data[center_channel].shape)
     r = np.hypot(x - center[0], y - center[1])
     boundary_pixel = boundary/pixel_size - center[0]
@@ -129,7 +150,7 @@ def plot_channel_maps(cube, imsize, tick_size, v_sys, sig):
     print("Plotted channel size is %dx%d arcseconds (~ %sx%s pixels)" %(imsize,imsize, extent*2, extent*2))
     print('Channels %d to %d will be plotted\n' %(skip_chans,last_chan))
 
-    ###plotting channel maps
+    # plotting channel maps
     print('Plotting tiled channel maps of %s...' %cube)
     print('Tile \t Channel \t Velocity(km/s)')
     for i in range(0,5):
@@ -138,7 +159,7 @@ def plot_channel_maps(cube, imsize, tick_size, v_sys, sig):
             vel = vels[skip_chans+channel]
             print('(%d,%d) \t\t %d \t\t %s' %(i, j, skip_chans+channel, '{0:7.1f}'.format(vel)))
 
-            ###writing velocity values to tiles
+            # writing velocity values to tiles
             axes[4-i][4-j].text(0.15, 0.875, '{0:7.1f}'.format(vel),
                             horizontalalignment='center',
                             verticalalignment='center',
@@ -146,54 +167,54 @@ def plot_channel_maps(cube, imsize, tick_size, v_sys, sig):
                             #fontweight='bold',
                             transform=axes[4-i][4-j].transAxes)
 
-            ###plotting channel maps
+            # plotting channel maps
             imgplot = axes[4-i][4-j].imshow(data[skip_chans+channel][low_lim:upp_lim,low_lim:upp_lim],cmap=cmap,
                                             extent=[-extent,extent,-extent,extent],origin="lower",vmax=max_int,vmin=min_int)
 
-            ###ploting contour at input specified sigma level
+            # ploting contour at input specified sigma level
             axes[4-i][4-j].contour(data[skip_chans+channel][low_lim:upp_lim,low_lim:upp_lim], [sig*av_rms], colors='white', linewidths=0.5, extent=[-extent,extent,-extent,extent], origin="lower")
 
-            ###setting ticks and empty ticklabels
+            # setting ticks and empty ticklabels
             axes[4-i][4-j].set_xticks(tick_locs)
             axes[4-i][4-j].set_yticks(tick_locs)
             axes[4-i][4-j].set_xticklabels([])
             axes[4-i][4-j].set_yticklabels([])
 
-            ###function call to change axes colour to white
+            # function call to change axes colour to white
             axes[4-i][4-j] = ChangeAxisColor(axes[4-i][4-j],'white')
 
-    ###setting ticklables for bottom-left corner tile
+    # setting ticklables for bottom-left corner tile
     axes[4][0].set_xticklabels(x_tick_labels)
     axes[4][0].set_yticklabels(y_tick_labels)
 
-    ###adding colourbar to plot
+    # adding colourbar to plot
     cbar_ax = fig.add_axes([0.9, 0.125, 0.025, 0.755])
     fig.colorbar(imgplot, cax=cbar_ax,orientation='vertical',ticklocation='right')
     cbar_ax.set_ylabel('Intensity (Jy/beam)', rotation=270, labelpad=15)
 
-    ###plotting beam patch
+    # plotting beam patch
     av_bmaj = np.median(beams['BMAJ'])/(3600*abs(CDELT1))
     av_bmin = np.median(beams['BMIN'])/(3600*abs(CDELT2))
     av_bpa  = np.median(beams['BPA'])
 
-    ###adding beam patch and bounding rectangle
+    # adding beam patch and bounding rectangle
     axes[4][0].add_patch(matplotlib.patches.Ellipse(xy=[-40,-40], width=av_bmaj, height=av_bmin, angle=(av_bpa-90), color='white', fill=True))
     axes[4][0].add_patch(matplotlib.patches.Rectangle(xy=[-40-10,-40-10], width=20, height=20, color='red', fill=False, linewidth=0.5))
     print('\nMedian synthesised beam size is %sx%s arcseconds\n' %('{0:.3f}'.format(np.median(beams['BMAJ'])), '{0:.3f}'.format(np.median(beams['BMIN']))))
 
-    ###adding legends for contours
+    # adding legends for contours
     line_contour = [Line2D([0],[0], color='white', lw=0.5)]
     label_contour = ['%s$\sigma$' %sig]
     axes[0][4].legend(line_contour, label_contour, loc='best', bbox_to_anchor=(0.95, 0.95))
 
-    ###adding plot title text
+    # adding plot title text
     fig.text(0.515, 0.92, 'Channel maps with %s$\sigma$ contours: %s' %(sig, cube[:-5]),
                             horizontalalignment='center',
                             verticalalignment='center',
                             fontsize=10, color='k',
                             fontweight='bold')
 
-    ###saving image to pdf file
+    # saving image to pdf file
     plt.savefig('./channel_maps/%s_channel_map.pdf' %cube[:-5], format='pdf', bbox_inches='tight')
     plt.close()
 
